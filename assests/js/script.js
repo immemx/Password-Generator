8b9e44fc33bd8c332d6c60e9874a0b2e6bc48eae
immemx <eayresm4@gmail.com>
2022-01-13 16:03:16 -0500

    Functioning Randomizer

---
+++ assests/js/script.js
@@ -0,0 +1,90 @@
+// Letters
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Special Characters
+var specialChara = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+// Numbers
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var upper case
+var lettersUpperCase = letters.map(name => name.toUpperCase());
+
+
+var generatePassword = function() {
+  //empty arr to fill with true conditions
+  var conditions = [];
+
+  // Find Length of password
+  var promptLength = parseInt(window.prompt('Choose a length between 8 - 128'));
+
+  //verify length is withing our range
+  if (promptLength >= 8 && promptLength <= 128) {
+    window.alert("Your password will be " + promptLength + " characters long.")
+  } else {
+    window.alert("Your password is invalid")
+    writePassword();
+  };
+
+    // Prompt LowerCase
+    var promptLowerCase = window.confirm('Do you want your password to contain lowercase characters?')
+
+    if (promptLowerCase) {
+      window.alert('Your password will contain lowercase characters')
+      conditions = conditions.concat(letters)
+    } else {
+      window.alert('Your password will NOT contain lowercase characters')
+    };
+
+    // Prompt uppercase
+    var promptUpperCase = window.confirm('Do you want your password to contain uppercase characters?')
+
+    if (promptUpperCase) {
+      window.alert('Your password will contain uppercase characters')
+      conditions = conditions.concat(lettersUpperCase)
+    } else {
+      window.alert('Your password will NOT contain uppercase characters')
+    };
+
+    // Prompt numbers
+    var promptnumbers = window.confirm('Do you want your password to contain numbers?')
+
+    if (promptnumbers) {
+      window.alert('Your password will contain numbers')
+      conditions = conditions.concat(numbers)
+    } else {
+      window.alert('Your password will NOT contain numbers')
+    };
+
+    // Prompt special Characters
+    var promptSpecialChara = window.confirm('Do you want your password to special characters?')
+
+    if (promptSpecialChara) {
+      window.alert('Your password will contain special characters')
+      conditions = conditions.concat(specialChara)
+    } else {
+      window.alert('Your password will NOT contain special characters')
+    };
+
+  var passwordCharacter = [];
+  for (var i = 0; i < promptLength; i++) {
+    var characters = conditions[Math.floor(Math.random() * conditions.length)];
+    passwordCharacter.push(characters)
+  }
+
+  var ps = passwordCharacter.join("");
+  console.log(ps)
+  return ps;
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
