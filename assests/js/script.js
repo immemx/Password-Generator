d1ceaa360acb4c2d7881a312809f2a0b1fd700e4
immemx <eayresm4@gmail.com>
2022-01-13 16:54:50 -0500

    loading bug fixed

--- assests/js/script.js
+++ assests/js/script.js
@@ -9,22 +9,9 @@ var lettersUpperCase = letters.map(name => name.toUpperCase());
 
 
 var generatePassword = function() {
+  //empty arr to fill with true conditions
+  var conditions = [];
 
-  var length = criteriaLength();
-  var criteria = criteria();
-  var passwordCharacter = [];
-
-
-  for (var i = 0; i < length; i++) {
-    var characters = criteria[Math.floor(Math.random() * criteria.length)];
-    passwordCharacter.push(characters)
-  }
-
-  var ps = passwordCharacter.join("");
-  return ps;
-}
-
-var criteriaLength = function() {
   // Find Length of password
   var promptLength = parseInt(window.prompt('Choose a length between 8 - 128'));
 
@@ -35,58 +22,57 @@ var criteriaLength = function() {
     window.alert("Your password is invalid")
     writePassword();
   };
-  
-  return;
-};
 
-var criteria = function() {
-  //empty arr to fill with true conditions
-  var conditions = [];
+    // Prompt LowerCase
+    var promptLowerCase = window.confirm('Do you want your password to contain lowercase characters?')
 
-  // Prompt LowerCase
-  var promptLowerCase = window.confirm('Do you want your password to contain lowercase characters?')
+    if (promptLowerCase) {
+      window.alert('Your password will contain lowercase characters')
+      conditions = conditions.concat(letters)
+    } else {
+      window.alert('Your password will NOT contain lowercase characters')
+    };
 
-  if (promptLowerCase) {
-    window.alert('Your password will contain lowercase characters')
-    conditions = conditions.concat(letters)
-  } else {
-    window.alert('Your password will NOT contain lowercase characters')
-  };
+    // Prompt uppercase
+    var promptUpperCase = window.confirm('Do you want your password to contain uppercase characters?')
 
-  // Prompt uppercase
-  var promptUpperCase = window.confirm('Do you want your password to contain uppercase characters?')
+    if (promptUpperCase) {
+      window.alert('Your password will contain uppercase characters')
+      conditions = conditions.concat(lettersUpperCase)
+    } else {
+      window.alert('Your password will NOT contain uppercase characters')
+    };
 
-  if (promptUpperCase) {
-    window.alert('Your password will contain uppercase characters')
-    conditions = conditions.concat(lettersUpperCase)
-  } else {
-    window.alert('Your password will NOT contain uppercase characters')
-  };
+    // Prompt numbers
+    var promptnumbers = window.confirm('Do you want your password to contain numbers?')
 
-  // Prompt numbers
-  var promptnumbers = window.confirm('Do you want your password to contain numbers?')
+    if (promptnumbers) {
+      window.alert('Your password will contain numbers')
+      conditions = conditions.concat(numbers)
+    } else {
+      window.alert('Your password will NOT contain numbers')
+    };
 
-  if (promptnumbers) {
-    window.alert('Your password will contain numbers')
-    conditions = conditions.concat(numbers)
-  } else {
-    window.alert('Your password will NOT contain numbers')
-  };
+    // Prompt special Characters
+    var promptSpecialChara = window.confirm('Do you want your password to special characters?')
 
-  // Prompt special Characters
-  var promptSpecialChara = window.confirm('Do you want your password to special characters?')
+    if (promptSpecialChara) {
+      window.alert('Your password will contain special characters')
+      conditions = conditions.concat(specialChara)
+    } else {
+      window.alert('Your password will NOT contain special characters')
+    };
 
-  if (promptSpecialChara) {
-    window.alert('Your password will contain special characters')
-    conditions = conditions.concat(specialChara)
-  } else {
-    window.alert('Your password will NOT contain special characters')
-  };
+  var passwordCharacter = [];
+  for (var i = 0; i < promptLength; i++) {
+    var characters = conditions[Math.floor(Math.random() * conditions.length)];
+    passwordCharacter.push(characters)
+  }
 
-  return conditions;
-};
-
-
+  var ps = passwordCharacter.join("");
+  console.log(ps)
+  return ps;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
