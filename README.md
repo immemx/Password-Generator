35c6b1a0eb31a36398c59856181e6bf40d180ad3
immemx <eayresm4@gmail.com>
2022-01-13 16:49:02 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Password Generator Starter Code
+# Password-Generator
+
+This application uses Javascript to generate a random password for its users.
+
+
+## Table of Contents
+
+1. [Installation](#installation) 
+2. [Usage](#usage)
+3. [License](#license)
+
+## Installation
+
+This application is deployed to github pages!
+
+you can either 
+
+Follow this link : https://immemx.github.io/Password-Generator/  
+~ OR ~  
+Use Git Clone to clone this code down to your local machine.
+
+## Usage
+
+Start by click the Red "Generate Password" button.  
+  
+![Generate-password-btn](assests/images/Generate-password-btn.jpg)  
+
+First you will enter the length of the password you would like to generate.  
+you mush choose between 8-128.  
+
+![Choose-length](assests/images/Choose-length.jpg)  
+
+Any amount not meeting this criteria will result in an error.
+
+![length-error](assests/images/length-error.jpg)  
+
+You will then answer a series of window alerts defining the criteria of your password.  
+
+Then SHAZAM! your highly secure, super personalized password is zapped into existance.
+
+## License 
+
+  Copyright 2021
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assests/js/script.js
+++ assests/js/script.js
@@ -9,9 +9,22 @@ var lettersUpperCase = letters.map(name => name.toUpperCase());
 
 
 var generatePassword = function() {
-  //empty arr to fill with true conditions
-  var conditions = [];
 
+  var length = criteriaLength();
+  var criteria = criteria();
+  var passwordCharacter = [];
+
+
+  for (var i = 0; i < length; i++) {
+    var characters = criteria[Math.floor(Math.random() * criteria.length)];
+    passwordCharacter.push(characters)
+  }
+
+  var ps = passwordCharacter.join("");
+  return ps;
+}
+
+var criteriaLength = function() {
   // Find Length of password
   var promptLength = parseInt(window.prompt('Choose a length between 8 - 128'));
 
@@ -22,57 +35,58 @@ var generatePassword = function() {
     window.alert("Your password is invalid")
     writePassword();
   };
+  
+  return;
+};
 
-    // Prompt LowerCase
-    var promptLowerCase = window.confirm('Do you want your password to contain lowercase characters?')
+var criteria = function() {
+  //empty arr to fill with true conditions
+  var conditions = [];
 
-    if (promptLowerCase) {
-      window.alert('Your password will contain lowercase characters')
-      conditions = conditions.concat(letters)
-    } else {
-      window.alert('Your password will NOT contain lowercase characters')
-    };
+  // Prompt LowerCase
+  var promptLowerCase = window.confirm('Do you want your password to contain lowercase characters?')
 
-    // Prompt uppercase
-    var promptUpperCase = window.confirm('Do you want your password to contain uppercase characters?')
+  if (promptLowerCase) {
+    window.alert('Your password will contain lowercase characters')
+    conditions = conditions.concat(letters)
+  } else {
+    window.alert('Your password will NOT contain lowercase characters')
+  };
 
-    if (promptUpperCase) {
-      window.alert('Your password will contain uppercase characters')
-      conditions = conditions.concat(lettersUpperCase)
-    } else {
-      window.alert('Your password will NOT contain uppercase characters')
-    };
+  // Prompt uppercase
+  var promptUpperCase = window.confirm('Do you want your password to contain uppercase characters?')
 
-    // Prompt numbers
-    var promptnumbers = window.confirm('Do you want your password to contain numbers?')
+  if (promptUpperCase) {
+    window.alert('Your password will contain uppercase characters')
+    conditions = conditions.concat(lettersUpperCase)
+  } else {
+    window.alert('Your password will NOT contain uppercase characters')
+  };
 
-    if (promptnumbers) {
-      window.alert('Your password will contain numbers')
-      conditions = conditions.concat(numbers)
-    } else {
-      window.alert('Your password will NOT contain numbers')
-    };
+  // Prompt numbers
+  var promptnumbers = window.confirm('Do you want your password to contain numbers?')
 
-    // Prompt special Characters
-    var promptSpecialChara = window.confirm('Do you want your password to special characters?')
+  if (promptnumbers) {
+    window.alert('Your password will contain numbers')
+    conditions = conditions.concat(numbers)
+  } else {
+    window.alert('Your password will NOT contain numbers')
+  };
 
-    if (promptSpecialChara) {
-      window.alert('Your password will contain special characters')
-      conditions = conditions.concat(specialChara)
-    } else {
-      window.alert('Your password will NOT contain special characters')
-    };
+  // Prompt special Characters
+  var promptSpecialChara = window.confirm('Do you want your password to special characters?')
 
-  var passwordCharacter = [];
-  for (var i = 0; i < promptLength; i++) {
-    var characters = conditions[Math.floor(Math.random() * conditions.length)];
-    passwordCharacter.push(characters)
-  }
+  if (promptSpecialChara) {
+    window.alert('Your password will contain special characters')
+    conditions = conditions.concat(specialChara)
+  } else {
+    window.alert('Your password will NOT contain special characters')
+  };
 
-  var ps = passwordCharacter.join("");
-  console.log(ps)
-  return ps;
-}
+  return conditions;
+};
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
